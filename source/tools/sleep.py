7c493d1c9f6aab1ee37885909f0f5450e01f2462
Quan <yonglelolu@foxmail.com>
2026-09-16 21:45:55 +0800

    perf: 优化内置延时逻辑

--- source/tools/sleep.py
+++ source/tools/sleep.py
@@ -8,8 +8,13 @@ def get_wait_time(
     sigma: float = 0.6,
 ) -> float:
     mu = log(avg_delay) - (sigma**2 / 2)
-    return max(0.5, lognormvariate(mu, sigma))
+    delay = lognormvariate(mu, sigma)
+    min_delay = avg_delay * 0.2
+    max_delay = avg_delay * 4.0
+    return max(min_delay, min(max_delay, delay))
 
 
-async def wait() -> None:
-    await sleep(get_wait_time())
+async def wait(
+    avg_delay: float | int = 6.0,
+) -> None:
+    await sleep(get_wait_time(avg_delay=avg_delay))
